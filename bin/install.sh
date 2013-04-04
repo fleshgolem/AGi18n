748d4d45e9722a1b58139118335670bbc9435565
Sören Busch <sbusch@mobivention.com>
2013-04-04 10:36:45 +0200

    changed install url

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Downloading genxibstrings..."
-curl https://raw.github.com/angelolloqui/AGi18n/master/bin/genxibstrings > /usr/local/bin/genxibstrings
+curl https://raw.github.com/fleshgolem/AGi18n/master/bin/genxibstrings > /usr/local/bin/genxibstrings
 chmod +x /usr/local/bin/genxibstrings
 
 echo "Downloading agi18n..."
-curl https://raw.github.com/angelolloqui/AGi18n/master/bin/agi18n > /usr/local/bin/agi18n
+curl https://raw.github.com/fleshgolem/AGi18n/master/bin/agi18n > /usr/local/bin/agi18n
 chmod +x /usr/local/bin/agi18n
 
 echo "Rehashing..."
